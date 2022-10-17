4846d3960186b407771f052490b06a8d8c2f1a2a
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2022-10-16 23:17:28 -0700

    upate reusable loader

--- components/ReusableLoader/ReusableLoader.jsx
+++ components/ReusableLoader/ReusableLoader.jsx
@@ -1,15 +1,12 @@
-import Layout from '../layout'
 import { Container, Center, Loader } from '@mantine/core'
 
 const ReusableLoader = () => {
     return (
-        <Layout>
-            <Container>
-                <Center>
-                    <Loader />
-                </Center>
-            </Container>
-        </Layout>
+        <Container>
+            <Center>
+                <Loader />
+            </Center>
+        </Container>
     )
 }
 
