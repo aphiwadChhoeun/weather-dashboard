df47f17fea826963c31f182d73d7a8c27c68007c
Aphiwad Chhoeun <aphiwad.chhoeun@gmail.com>
2022-10-16 23:14:26 -0700

    add caching

--- hooks/useLocation.js
+++ hooks/useLocation.js
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
 export const fetchLatLng = async (city) => {
-    const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${'15f7316707a06d2fa846b57979e19855'}`);
+    const res = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${'15f7316707a06d2fa846b57979e19855'}`);
     return await res.json();
 }
 
--- pages/api/dashboard.js
+++ pages/api/dashboard.js
@@ -1,6 +1,10 @@
 import locations from '../../data/locations.json'
 
 const handler = (req, res) => {
+    res.setHeader(
+        'Cache-Control',
+        'public, s-maxage=10, stale-while-revalidate=59'
+    )
     res.status(200).json({
         data: locations
     })
